400369f8eb38b77f51f2ced6e0c5e8d113831354
`thais` <`tjlannes@gmail.com`>
2023-01-26 09:49:43 +0100

    fix code smell in airlines pro

--- challenge-airlines-pro/Proyecto 4 - AirlinesPRO.js
+++ challenge-airlines-pro/Proyecto 4 - AirlinesPRO.js
@@ -1,133 +1,192 @@
-let withScale = []
+let withScale = [];
 
-let userName = ""
+let userName = "";
 
 const flights = [
-    { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
-    { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-    { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-    { id: 03, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-    { id: 04, to: "London", from: "Madrid", cost: 200, scale: false },
-    { id: 05, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-    { id: 06, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-    { id: 07, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-    { id: 08, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-    { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
+  { id: 0, to: "New York", from: "Barcelona", cost: 700, scale: false },
+  { id: 1, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+  { id: 2, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+  { id: 3, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+  { id: 4, to: "London", from: "Madrid", cost: 200, scale: false },
+  { id: 5, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+  { id: 6, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+  { id: 7, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+  { id: 8, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+  { id: 9, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
-  
-const askForNameAndGreet = () => {
-    userName = prompt('Welcome to Airlines ISDI Coders! May I have your name?')
-    if (userName === "" || userName === null) {
-        userName = "passenger"
-    }
-    window.alert(`Hello ${userName}! Let me help you with your next trip.\nLet's check the available flights:`)
-}
 
+const askForNameAndGreet = () => {
+  userName = prompt("Welcome to Airlines ISDI Coders! May I have your name?");
+  if (userName === "" || userName === null) {
+    userName = "passenger";
+  }
+  window.alert(
+    `Hello ${userName}! Let me help you with your next trip.\nLet's check the available flights:`
+  );
+};
 
 const showFlights = () => {
-    console.log("Here are the flights:")
-    console.log("-------------------------------------------------------------------------------------------✈")
-    for (let i = 0; i < flights.length; i++) { 
-        if (flights[i].scale === true) {
-            withScale.push(flights[i]);
-            console.log(`The flight ${flights[i].id} with origin: ${flights[i].from} and destination: ${flights[i].to} has a cost of ${flights[i].cost.toLocaleString('es-ES', {style: 'currency', currency: 'EUR'})}, with a stopover.`)
-        } else {
-    console.log(`The flight ${flights[i].id} with origin: ${flights[i].from} and destination: ${flights[i].to} has a cost of ${flights[i].cost.toLocaleString('es-ES', {style: 'currency', currency: 'EUR'})}, without stopover.`)};
-    }console.log("-------------------------------------------------------------------------------------------✈")
+  console.log("Here are the flights:");
+  console.log(
+    "-------------------------------------------------------------------------------------------✈"
+  );
+  for (let i = 0; i < flights.length; i++) {
+    if (flights[i].scale === true) {
+      withScale.push(flights[i]);
+      console.log(
+        `The flight ${flights[i].id} with origin: ${
+          flights[i].from
+        } and destination: ${flights[i].to} has a cost of ${flights[
+          i
+        ].cost.toLocaleString("es-ES", {
+          style: "currency",
+          currency: "EUR",
+        })}, with a stopover.`
+      );
+    } else {
+      console.log(
+        `The flight ${flights[i].id} with origin: ${
+          flights[i].from
+        } and destination: ${flights[i].to} has a cost of ${flights[
+          i
+        ].cost.toLocaleString("es-ES", {
+          style: "currency",
+          currency: "EUR",
+        })}, without stopover.`
+      );
+    }
+  }
+  console.log(
+    "-------------------------------------------------------------------------------------------✈"
+  );
 };
 
 const askForRole = () => {
-    const role = prompt(`Dear ${userName}, are you a USER or ADMIN?`)
-    if(role === null) {
-        askForRole()
-    } else if(
-        role.toUpperCase() !== 'USER' && role.toUpperCase() !== 'ADMIN'){ 
-        askForRole()
-    } else {
-        return role.toUpperCase() 
-    }
-}
+  const role = prompt(`Dear ${userName}, are you a USER or ADMIN?`);
+  if (role === null) {
+    askForRole();
+  } else if (role.toUpperCase() !== "USER" && role.toUpperCase() !== "ADMIN") {
+    askForRole();
+  } else {
+    return role.toUpperCase();
+  }
+};
 
-const lastFlights = () => {  
-    let lastQuestion = confirm(`Dear ${userName}, would you like to view the last 5 flights of the day?`);
-  
-    if (lastQuestion){
-        console.log(`✈ The last five flights today are:`)
-        const lastFlights = flights.slice(5)
+const lastFlights = () => {
+  let lastQuestion = confirm(
+    `Dear ${userName}, would you like to view the last 5 flights of the day?`
+  );
+
+  if (lastQuestion) {
+    console.log(`✈ The last five flights today are:`);
+    const lastFlights = flights.slice(5);
     for (let i = 0; i < lastFlights.length; i++) {
-        console.log(`Departure from ${lastFlights[i].from} to ${lastFlights[i].to}.`)};
-}}
-
-const askForActionAdmin = () => { 
-    const action = prompt('Do you want to CREATE a new flight or DELETE? [CREATE/DELETE]')
-    if(action === null || action === "") {
-        askForActionAdmin()
-    } else if(
-        action.toUpperCase() !== 'CREATE' && action.toUpperCase() !== 'DELETE'){
-        askForActionAdmin()
-    } else {
-        return action.toUpperCase()
+      console.log(
+        `Departure from ${lastFlights[i].from} to ${lastFlights[i].to}.`
+      );
     }
-}
+  }
+};
+
+const askForActionAdmin = () => {
+  const action = prompt(
+    "Do you want to CREATE a new flight or DELETE? [CREATE/DELETE]"
+  );
+  if (action === null || action === "") {
+    askForActionAdmin();
+  } else if (
+    action.toUpperCase() !== "CREATE" &&
+    action.toUpperCase() !== "DELETE"
+  ) {
+    askForActionAdmin();
+  } else {
+    return action.toUpperCase();
+  }
+};
 
 const adminRole = () => {
-    const adminRoleResult = askForActionAdmin()
-    if (adminRoleResult === 'CREATE'){
-        if (flights.length >= 15 + 1) {
-            window.alert("It has exceeded the number of flights for today.");
-        }
-        let newFlight = {}
-        newFlight.id = flights.length, -1, + 1
-        newFlight.to = prompt('Where does the flight go?')
-        newFlight.from = prompt('Where does the flight depart from?')
-        newFlight.cost = prompt('How much is the ticket for this flight?')
-        newFlight.scale = prompt("Are there any stopovers on this flight, true or false? [true/false]")
-        if (newFlight.scale === "true") {
-            newFlight.scale = true
-        } else {
-            newFlight.scale = false}
-        flights.push(newFlight) 
-        window.alert (`The flight has been successfully created.`)
-        console.log(`✈ Update: New flight added.`)
-        showFlights()
-        adminRole()
-    } else { 
-        const flightsToDelete = +prompt('Enter the ID of the flight you wish to delete:')
-        flights.splice(flightsToDelete, 1) 
-        window.alert (`The flight with ID ${flightsToDelete} has been successfully deleted.`)
-        console.log(`✈ Update: Flight with ID ${flightsToDelete} has been deleted.`)
-        showFlights()
-        window.alert("Thanks for visiting our airline. See you soon!")
+  const adminRoleResult = askForActionAdmin();
+  if (adminRoleResult === "CREATE") {
+    if (flights.length >= 15 + 1) {
+      window.alert("It has exceeded the number of flights for today.");
     }
-}
+    let newFlight = {};
+    (newFlight.id = flights.length), -1, +1;
+    newFlight.to = prompt("Where does the flight go?");
+    newFlight.from = prompt("Where does the flight depart from?");
+    newFlight.cost = prompt("How much is the ticket for this flight?");
+    newFlight.scale = prompt(
+      "Are there any stopovers on this flight, true or false? [true/false]"
+    );
+    if (newFlight.scale === "true") {
+      newFlight.scale = true;
+    } else {
+      newFlight.scale = false;
+    }
+    flights.push(newFlight);
+    window.alert(`The flight has been successfully created.`);
+    console.log(`✈ Update: New flight added.`);
+    showFlights();
+    adminRole();
+  } else {
+    const flightsToDelete = +prompt(
+      "Enter the ID of the flight you wish to delete:"
+    );
+    flights.splice(flightsToDelete, 1);
+    window.alert(
+      `The flight with ID ${flightsToDelete} has been successfully deleted.`
+    );
+    console.log(
+      `✈ Update: Flight with ID ${flightsToDelete} has been deleted.`
+    );
+    showFlights();
+    window.alert("Thanks for visiting our airline. See you soon!");
+  }
+};
 
 const userRole = () => {
-    const askForPrice = +prompt(`✈ Price filter\nDear ${userName}, what is your budget?`)
-    if (askForPrice === null) {
-        window.alert("Thanks for visiting our airline. See you soon!")
-    } console.log("✈ See below for flights that fit your budget:")
-    const flightsForUser = []
-    flights.forEach((flight) => {
-        if(flight.cost <= askForPrice){
-            flightsForUser.push(flight)
-            console.log(`The flight ${flight.id} with origin: ${flight.from} and destination: ${flight.to} has a cost of ${flight.cost.toLocaleString('es-ES', {style: 'currency', currency: 'EUR'})}.`)
-        }
-    })
-    window.alert("Thanks for visiting our airline. See you soon!")
-}
+  const askForPrice = +prompt(
+    `✈ Price filter\nDear ${userName}, what is your budget?`
+  );
+  if (askForPrice === null) {
+    window.alert("Thanks for visiting our airline. See you soon!");
+  }
+  console.log("✈ See below for flights that fit your budget:");
+  const flightsForUser = [];
+  flights.forEach((flight) => {
+    if (flight.cost <= askForPrice) {
+      flightsForUser.push(flight);
+      console.log(
+        `The flight ${flight.id} with origin: ${flight.from} and destination: ${
+          flight.to
+        } has a cost of ${flight.cost.toLocaleString("es-ES", {
+          style: "currency",
+          currency: "EUR",
+        })}.`
+      );
+    }
+  });
+  window.alert("Thanks for visiting our airline. See you soon!");
+};
 
 const playAirline = () => {
-    askForNameAndGreet()
-    showFlights()
-    const average = flights.reduce((total, next) => total + next.cost, 0) / flights.length; //to calculate the average
-    console.log(`✈ The average price of flights is ${average.toLocaleString('es-ES', {style: 'currency', currency: 'EUR'})} and there are ${withScale.length} flights with stopovers.`);
-    lastFlights()
-    const askUserOrAdmin = askForRole()
-    if (askUserOrAdmin === 'ADMIN') {
-        adminRole()
-    } else {
-        userRole()
-    }
-}
+  askForNameAndGreet();
+  showFlights();
+  const average =
+    flights.reduce((total, next) => total + next.cost, 0) / flights.length; //to calculate the average
+  console.log(
+    `✈ The average price of flights is ${average.toLocaleString("es-ES", {
+      style: "currency",
+      currency: "EUR",
+    })} and there are ${withScale.length} flights with stopovers.`
+  );
+  lastFlights();
+  const askUserOrAdmin = askForRole();
+  if (askUserOrAdmin === "ADMIN") {
+    adminRole();
+  } else {
+    userRole();
+  }
+};
 
-playAirline()
+playAirline();
--- challenge-bingo/Proyecto 5 - Bingo.js
+++ challenge-bingo/Proyecto 5 - Bingo.js
@@ -1,132 +1,138 @@
-const bingoCard = []
+const bingoCard = [];
 
-line1 = []
-line2 = []
-line3 = []
+line1 = [];
+line2 = [];
+line3 = [];
 
-let round = 0
+let round = 0;
 
-let calledNumbers = []
+let calledNumbers = [];
 
-let checkBingo = []
+let checkBingo = [];
 
-const askForUserNameAndGreet =() => { //bienvenida
-    const userName = prompt("Welcome to Bingo! Please, enter your name."
-    ); if(!userName){
-        alert("I'm sure you have a beautiful name. Please enter your name.")
-        askForUserNameAndGreet()
-    } else{
-    console.log(`Hello ${userName}!`)
-    alert(`Hi ${userName}! Take a look in your card.`)
-    }
-}
-
-const randomNumbersCard = () => { //crea numeros randoms 
-    let randomNumber = Math.ceil(Math.random() * 99) + 1
-    return randomNumber
+const askForUserNameAndGreet = () => {
+  //bienvenida
+  const userName = prompt("Welcome to Bingo! Please, enter your name.");
+  if (!userName) {
+    alert("I'm sure you have a beautiful name. Please enter your name.");
+    askForUserNameAndGreet();
+  } else {
+    console.log(`Hello ${userName}!`);
+    alert(`Hi ${userName}! Take a look in your card.`);
+  }
 };
 
-const createCard = () => { //crea el carton
-    while (bingoCard.length < 15) {
-        const bingoCardNumbers = {
-            number: randomNumbersCard(), matched: false,
-        }
-        if (calledNumbers.includes(bingoCardNumbers.number)) {
-            randomNumbersCard()
-        } 
-        else {
-            calledNumbers.push(bingoCardNumbers.number);
-            bingoCard.push(bingoCardNumbers)
-        }
-    }
-    return showAndConfirmCard()
+const randomNumbersCard = () => {
+  //crea numeros randoms
+  let randomNumber = Math.ceil(Math.random() * 99) + 1;
+  return randomNumber;
 };
 
-const showAndConfirmCard = () => { //enseña y pregunta al usuario si desea el carton
-    console.log("Your numbers are:")
-    console.table(bingoCard)
-    console.log("**************")
-    
-    let confirmCard = confirm("Do you want change your card?\n\nACCEPT, if yes\nCANCEL, to start the game\n")
-    if (confirmCard === true){
-        deleteCard()
-        createCard()
+const createCard = () => {
+  //crea el carton
+  while (bingoCard.length < 15) {
+    const bingoCardNumbers = {
+      number: randomNumbersCard(),
+      matched: false,
+    };
+    if (calledNumbers.includes(bingoCardNumbers.number)) {
+      randomNumbersCard();
+    } else {
+      calledNumbers.push(bingoCardNumbers.number);
+      bingoCard.push(bingoCardNumbers);
     }
-    else {
-        line1 = bingoCard.slice(0, 5)
-        line2 = bingoCard.slice(5, 10)
-        line3 = bingoCard.slice(10, 15)
-        alert("Perfect! Let's go to the first number.\nMay the luck be with you!")
+  }
+  return showAndConfirmCard();
+};
+
+const showAndConfirmCard = () => {
+  //enseña y pregunta al usuario si desea el carton
+  console.log("Your numbers are:");
+  console.table(bingoCard);
+  console.log("**************");
+
+  let confirmCard = confirm(
+    "Do you want change your card?\n\nACCEPT, if yes\nCANCEL, to start the game\n"
+  );
+  if (confirmCard === true) {
+    deleteCard();
+    createCard();
+  } else {
+    line1 = bingoCard.slice(0, 5);
+    line2 = bingoCard.slice(5, 10);
+    line3 = bingoCard.slice(10, 15);
+    alert("Perfect! Let's go to the first number.\nMay the luck be with you!");
+  }
+};
+
+const callNumber = () => {
+  //llama a un numero aleatorio
+  let newNumber = Math.ceil(Math.random() * 99) + 1;
+  while (calledNumbers.includes(newNumber) || newNumber === 0) {
+    newNumber = Math.ceil(Math.random() * 99) + 1;
+  }
+  calledNumbers.push(newNumber);
+  round++;
+  return newNumber;
+};
+
+Array.prototype.indexOfNumber = function (number) {
+  for (var i = 0; i < this.length; i++) if (this[i].number === number) return i;
+  return -1;
+};
+
+const cleanCalledNumbers = () => (calledNumbers = []);
+
+const checkNumberCard = () => {
+  //verifica si el numero coincide con el carton
+  const bingoNumber = callNumber();
+  for (let i = 0; i < bingoCard.length; i++) {
+    let index = bingoCard.indexOfNumber(bingoNumber);
+    if (index !== -1) {
+      bingoCard[index].matched = true;
+      bingoCard[index].number = "X";
+      alert(
+        `The number is: ${bingoNumber}...\nWait... WOW! You have this number!`
+      );
+      console.log(`The number ${bingoNumber} has been found!`);
+      checkBingo.push(bingoNumber);
+      showCard();
+      return checkNumberCard();
+    } else {
+      alert(`The number is: ${calledNumbers.slice(-1)}`);
+      return checkNumberCard();
     }
-}       
+  }
+};
 
-const callNumber = () => { //llama a un numero aleatorio
-    let newNumber = Math.ceil(Math.random() * 99) + 1
-    while (calledNumbers.includes(newNumber)||newNumber === 0) {
-        newNumber = Math.ceil(Math.random() * 99) + 1
-    }
-    calledNumbers.push(newNumber)
-    round++
-    return newNumber
-}
-
-Array.prototype.indexOfNumber = function(number) {
-    for (var i = 0; i < this.length; i++)
-        if (this[i].number === number)
-            return i;
-    return -1;
-}        
-
-const cleanCalledNumbers = () => calledNumbers = []
-
-
-const checkNumberCard = () => { //verifica si el numero coincide con el carton
-    const bingoNumber = callNumber()
-    for (let i = 0; i < bingoCard.length; i++) {
-        var index = bingoCard.indexOfNumber(bingoNumber);
-        if (index !== -1){
-            bingoCard[index].matched = true;
-            bingoCard[index].number = "X";
-            alert(`The number is: ${bingoNumber}...\nWait... WOW! You have this number!`)
-            console.log(`The number ${bingoNumber} has been found!`)
-            checkBingo.push(bingoNumber)
-            showCard()
-            return checkNumberCard()
-        }
-        else {
-            alert(`The number is: ${calledNumbers.slice(-1)}`)
-            return checkNumberCard()
-        }
-    }
-}
-
-const showCard = () => { //enseña el carton
-    console.table(bingoCard)
-}
-
+const showCard = () => {
+  //enseña el carton
+  console.table(bingoCard);
+};
 
 const nextTurn = () => confirm("Keep playing?");
 
-const deleteCard = () => { //borrar carton
-    calledNumbers.length = 0
-    bingoCard.length = 0
-}
+const deleteCard = () => {
+  //borrar carton
+  calledNumbers.length = 0;
+  bingoCard.length = 0;
+};
 
 const playBingo = () => {
-    askForUserNameAndGreet()
-    createCard()
-    cleanCalledNumbers()
-    
-    do {
-        checkNumberCard()
-        if (checkBingo.length === 15) { 
-            console.log("BINGO!!!!")
-            break
-        }
-    } while (nextTurn());
-    alert(
-        `You finished the game in ${round} rounds.\nIt was great to play with you! See you next time, ${userName}.`
-    );
-}
+  askForUserNameAndGreet();
+  createCard();
+  cleanCalledNumbers();
 
-playBingo() //numeros repetidos nos bombo, nao ta fazendo bingo
+  do {
+    checkNumberCard();
+    if (checkBingo.length === 15) {
+      console.log("BINGO!!!!");
+      break;
+    }
+  } while (nextTurn());
+  alert(
+    `You finished the game in ${round} rounds.\nIt was great to play with you! See you next time, ${userName}.`
+  );
+};
+
+playBingo(); //numeros repetidos nos bombo, nao ta fazendo bingo
